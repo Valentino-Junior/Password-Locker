8d9675d47a711a01d4bf991d14fa63a59fb8fe9a
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-26 18:53:28 +0300

    Corrected the indention

--- run.py
+++ run.py
@@ -4,15 +4,15 @@ from user import User
 import random
 import string
 
+# credential methods
 
- #credential methods
-def create_credential(site_name, user_name, password ):
+
+def create_credential(site_name, user_name, password):
     '''
     Function to create a new user credentials
     '''
-    new_credential = Credential(site_name, user_name, password )
+    new_credential = Credential(site_name, user_name, password)
     return new_credential
-
 
 
 def save_credential(credential):
@@ -21,11 +21,13 @@ def save_credential(credential):
     '''
     credential.save_credential()
 
+
 def delete_credential(credential):
     '''
     Function to delete a credential
     '''
     credential.delete_credential()
+
 
 def find_credential(site_name):
     '''
@@ -33,11 +35,13 @@ def find_credential(site_name):
     '''
     return Credential.locate_account(site_name)
 
+
 def check_existing_credentials(account_name):
     '''
     Function to check whether a credential exists
     '''
     return Credential.credential_exist(account_name)
+
 
 def display_credentials():
     '''
@@ -46,14 +50,14 @@ def display_credentials():
     return Credential.display_credentials()
 
 
-
-#Users method
+# Users method
 def create_user(first_name, last_name, password):
     '''
    To generate a new user credential, use this function.
     '''
     new_user = User(first_name, last_name, password)
     return new_user
+
 
 def save_users(user):
     '''
@@ -63,18 +67,17 @@ def save_users(user):
 
 
 def main():
-  
+
     print("HELLO, WELCOME TO PASSWORD LOCKER.")
     usr_name = input("Please enter your name:")
 
     while True:
-        print('*'*60)
-        print(f"Hello {usr_name}, Kindly use these short codes to either sign up or login to your account.") 
+        print(
+            f"Hello {usr_name}, Kindly use these short codes to either sign up or login to your account.")
         print("sn - sign up to new users")
         print("lg - log into your account")
         short_code = input("Enter short code:").upper()
 
-        
         if short_code == 'SN':
             print("Enter  first_name: ")
             fst_name = input()
@@ -84,29 +87,27 @@ def main():
 
             print("Enter password")
             fpin = input()
-            print('*' *60)
+            print('*' * 60)
 
-            
-
-            print("You've successfully completed the registration process.",)
+            print("You've successfully completed the registration process.")
             print("Kindly, proceed by logging into your account",)
             print('\n')
-            print('*' *60)    
 
+        elif short_code == 'LG':
             print("Enter your username")
             username = input()
-           
+
             print("Enter your password")
             pin = input()
-            if f"{fst_name }. {lst_name} == username and fpin == pin":
+            if f"{username } = {pin}":
+                print('*' * 60)
+
                 print(f"{username}, you've succesfully logged into your account")
                 print('\n')
 
-                print('*' *60)
-
                 pass
                 while True:
-                    print("Use these short codes: sc - Save credentials \n, cc - Create new credentials \n, dc - display credentials \n, lc - locate saved credential \n, del - delete credentials \n, ex - exit the account")
+                    print("Use these short codes: \n sc - Save credentials \n, cc - Create new credentials \n, dc - display credentials \n, lc - locate saved credential \n, del - delete credentials \n, ex - exit the account")
 
                     short_code = input()
 
@@ -120,27 +121,29 @@ def main():
                         print(" Input User Name: ")
                         user_name = input()
 
-                        
-
-                        print("Would you prefer a password that is customized for you?, respond by  YES or NO")
+                        print(
+                            "Would you prefer a password that is customized for you?, respond by  YES or NO")
                         password = input().upper()
                         if password == 'YES':
                             print("What length do you want your password to be?")
                             password_length = int(input())
                             chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
-                            gen_password= "".join(random.choice(chars) for i in range(password_length))
-                       
+                            gen_password = "".join(random.choice(
+                                chars) for i in range(password_length))
+
                         elif password == 'NO':
                             print("Enter the password you would like to use.")
-                            gen_password= input()
+                            gen_password = input()
 
                         else:
                             print("Wrong input, enter yes or no",)
 
-                        save_credential(create_credential(site_name, user_name, gen_password)) 
+                        save_credential(create_credential(
+                            site_name, user_name, gen_password))
                         print('\n')
                         print('-'*50)
-                        print(f"{user_name}, your new password has successfully  been created" )
+                        print(
+                            f"{user_name}, your new password has successfully  been created")
                         print(f" Your generated password is: {gen_password}")
                         print('-'*50)
                         print('\n')
@@ -155,27 +158,26 @@ def main():
                         print(" Input User Name: ")
                         user_name = input()
 
-
                         print("Input Password: ")
                         password = input()
 
-                        
-                    
-                        save_credential(create_credential(site_name, user_name, password)) 
+                        save_credential(create_credential(
+                            site_name, user_name, password))
                         print('\n')
                         print(f"{site_name} credentials successfully saved")
                         print('\n')
 
-
                     elif short_code == 'dc':
                         if display_credentials():
-                            print("The following is a list of all of your credentials.")
+                            print(
+                                "The following is a list of all of your credentials.")
                             print('\n')
 
                             for credential in display_credentials():
                                 print(f"{credential.account_name}")
                                 print("-"*50)
-                                print(f"Username: {credential.first_name} {credential.last_name}")
+                                print(
+                                    f"Username: {credential.first_name} {credential.last_name}")
                                 print(f"Password: {credential.user_password}")
                                 print('\n')
 
@@ -185,12 +187,14 @@ def main():
                             print('\n')
 
                     elif short_code == 'lc':
-                        print("Please type the name of the site you're looking for into the below.\n")
+                        print(
+                            "Please type the name of the site you're looking for into the below.\n")
 
                         search_site = input("Site Name: ")
                         if check_existing_credentials(search_site):
                             search_site = find_credential(search_site)
-                            print(f"{search_site.first_name} {search_site.last_name}")
+                            print(
+                                f"{search_site.first_name} {search_site.last_name}")
                             print("-"*50)
 
                             print(f"Password: {search_site.user_password}")
@@ -200,7 +204,8 @@ def main():
                             print('\n')
 
                     elif short_code == 'del':
-                        print("Please type the name of the site you wish to remove below.\n")
+                        print(
+                            "Please type the name of the site you wish to remove below.\n")
                         site_delete = input()
                         if check_existing_credentials(site_delete):
                             site_delete = find_credential(site_delete)
@@ -211,27 +216,14 @@ def main():
                         else:
                             print("There are no credentials.")
 
-
-                    elif short_code == "ex":
-                        print("THANK YOU FOR USING PASSWORD LOCKER AND WELCOME BACK AGAIN!!",)
-                        print('\n')
                         print('-'*50)
+                    elif short_code == "ex":
+                        print("-"*50)
+
+                        print(
+                            "THANK YOU FOR USING PASSWORD LOCKER AND WELCOME BACK AGAIN!!",)
+                        print('\n')
                         break
-            else:
-                print("Login credentials are invalid.")
-                print('\n')
 
-        # elif short_code == 'lg':
-        #     print("Please Enter your name")
-        #     inputname = input()
-
-        #     print("Enter Your Password")
-        #     inputpass = input()
-
-            # print("The login information you submitted seems not to be valid. Before you can log in, you must first register an account.")
-            # print('\n')
-   
-
-        
 if __name__ == '__main__':
     main()
