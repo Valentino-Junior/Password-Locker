a311026f275a94271655bf3d7044b45a829f3b8d
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-26 11:23:39 +0300

    Defined the credenial functions

--- run.py
+++ run.py
@@ -5,45 +5,45 @@ import random
 import string
 
 
+ #credential methods
+def create_credential(site_name, user_name, password ):
+    '''
+    Function to create a new user credentials
+    '''
+    new_credential = Credential(site_name, user_name, password )
+    return new_credential
 
-# def create_credential(site_name, user_name, password ):
-#     '''
-#     Function to create a new user credentials
-#     '''
-#     new_credential = Credential(site_name, user_name, password )
-#     return new_credential
 
 
+def save_credential(credential):
+    '''
+    Funtion to save the credential
+    '''
+    credential.save_credential()
 
-# def save_credential(credential):
-#     '''
-#     Funtion to save the credential
-#     '''
-#     credential.save_credential()
+def delete_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credential()
 
-# def delete_credential(credential):
-#     '''
-#     Function to delete a credential
-#     '''
-#     credential.delete_credential()
+def find_credential(site_name):
+    '''
+    Function to find a credential
+    '''
+    return Credential.locate_account(site_name)
 
-# def find_credential(site_name):
-#     '''
-#     Function to find a credential
-#     '''
-#     return Credential.locate_account(site_name)
+def check_existing_credentials(account_name):
+    '''
+    Function to check whether a credential exists
+    '''
+    return Credential.credential_exist(account_name)
 
-# def check_existing_credentials(account_name):
-#     '''
-#     Function to check whether a credential exists
-#     '''
-#     return Credential.credential_exist(account_name)
-
-# def display_credentials():
-#     '''
-#     Function that returns all the saved credentials
-#     '''
-#     return Credential.display_credentials()
+def display_credentials():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credential.display_credentials()
 
 
 
