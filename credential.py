ec4155759b9baaac572fd316bba6a82a8922ab00
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-26 05:07:39 +0300

    Added classmethod in the credential class

--- credential.py
+++ credential.py
@@ -9,3 +9,16 @@ class Credential:
         self.user_name = user_name
         self.password = password
         self.email = email
+
+    def save_credential(self):
+        """
+        Credential objects are saved in credential array via the save contact function.
+        """
+        Credential.credential_array.append(self)
+
+    @classmethod
+    def display_credential(cls):
+        """
+        The credential array is returned by this function.
+        """
+        return cls.credential_array
