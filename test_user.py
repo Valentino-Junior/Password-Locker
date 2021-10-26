147a43c0914b6e05ef95b1d4329dc9d5d8048e5b
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-26 15:12:34 +0300

    Conducted test on credential file

--- test_user.py
+++ test_user.py
@@ -1,43 +1,43 @@
-# import unittest
-# from user import User
+import unittest
+from user import User
 
-# class Test_user(unittest.TestCase):
-#     '''
-#     A test class that describes a testcase for the user class's behavior.
-#     '''
+class Test_user(unittest.TestCase):
+    '''
+    A test class that describes a testcase for the user class's behavior.
+    '''
 
-#     def setUp(self):      
-#         self.new_user_info = User("Valentino","Junior", "1234")
+    def setUp(self):      
+        self.new_user_info = User("Valentino","Junior", "1234")
        
 
 
-#     def tearDown(self):
-#         '''
-#         Tear down method cleans up after each test case has run
-#         '''
-#         User.users_entry = []
+    def tearDown(self):
+        '''
+        Tear down method cleans up after each test case has run
+        '''
+        User.users_entry = []
         
 
-#     def test__init(self):
-#         '''
-#         After each test case has completed, the tear down method cleans up.
-#         '''
+    def test__init(self):
+        '''
+        After each test case has completed, the tear down method cleans up.
+        '''
         
-#         self.assertEqual(self.new_user_info.first_name,"Valentino")
-#         self.assertEqual(self.new_user_info.last_name,"Junior")
-#         self.assertEqual(self.new_user_info.password,"1234")
+        self.assertEqual(self.new_user_info.first_name,"Valentino")
+        self.assertEqual(self.new_user_info.last_name,"Junior")
+        self.assertEqual(self.new_user_info.password,"1234")
 
-#     def test_save_users(self):
-#         '''
-#         It is done to check whether the item has been stored in the users list.
-#         '''
-#         self.new_users.save_user()
-#         self.assertEqual(len(User.users_entry),1)
+    def test_save_users(self):
+        '''
+        It is done to check whether the item has been stored in the users list.
+        '''
+        self.new_users.save_user()
+        self.assertEqual(len(User.users_entry),1)
 
     
 
   
 
 
-# if __name__ == '__main_':
-#     unittest.main()
+if __name__ == '__main_':
+    unittest.main()
